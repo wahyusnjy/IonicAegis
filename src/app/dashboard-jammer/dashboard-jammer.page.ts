7e61e5f39cde088bd439158e20f1723c8e12d8a4
MacBook <wahyusnjy@gmail.com>
2024-01-05 18:15:24 +0700

    Tambah logic Dashboard Jammer Popup Count

--- src/app/dashboard-jammer/dashboard-jammer.page.ts
+++ src/app/dashboard-jammer/dashboard-jammer.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+// import * as data from '../data/jammer.json';
+import dashboardJammer, { jammer1, jammer2 } from '../data/jammer.json';
+
 
 @Component({
   selector: 'app-dashboard-jammer',
@@ -7,19 +10,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard-jammer.page.scss'],
 })
 export class DashboardJammerPage implements OnInit {
+
+  Datajson: any = [];
+  Jammer1: any = [];
+  Jammer2: any = [];
+
   channelJam1: number[] = [1,2,3,4,5,6];
   channelJam2: number[] = [7,8,9,10,11,12];
   selectJam1: number = 1;
   selectJam2: number = 7;
 
-  channelJam1Tabs: { channel: number; isActive: boolean }[] = [
-    { channel: 1, isActive: true },
-    { channel: 2, isActive: false },
-    { channel: 3, isActive: false },
-    { channel: 4, isActive: false },
-    { channel: 5, isActive: false },
-    { channel: 6, isActive: false },
-  ];
 
   channelJam2Tabs: { channel: number; isActive: boolean }[] = [
     { channel: 7, isActive: true },
@@ -33,15 +33,93 @@ export class DashboardJammerPage implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.getDataJson();
+  }
+// Hitung BW Freq Start
+calculateBwFreqStart(jammer: any): string {
+  let bwFreqStart: any = 0;
+  const bw = jammer.bw / 2;
+
+  if (bw > jammer.freq) {
+    bwFreqStart = (bw - jammer.freq) / 1000000;
+  } else if (bw < jammer.freq) {
+    bwFreqStart = (jammer.freq - bw) / 1000000;
+  }
+  return `${bwFreqStart}`;
+}
+
+// Hitung BW Freq End
+calculateBwFreqEnd(jammer: any): string {
+  let bwFreqEnd: any = 0;
+  const bw = jammer.bw / 2;
+
+  bwFreqEnd = (bw + jammer.freq) / 1000000;
+  return `${bwFreqEnd}`;
+}
+
+//Freq center
+FreqCenter(jammer: any): string{
+  const freq = jammer.freq / 1000000;
+  return `${freq}`;
+}
+
+// Freq Span
+FreqSpan(jammer: any): string {
+  const freqSpan = jammer.bw / 1000000;
+  return `${freqSpan}`;
+}
+
+// RFAtt
+RFAtt(jammer:any): string {
+  const RfAtt = jammer.att / 100;
+  return `${RfAtt}`;
+}
+
+countT(jammer:any): string {
+  const countT = jammer.fs / 1000000;
+  return `${countT}`;
+}
+
+countV(jammer:any): string {
+  const countV = jammer.voltage / 1000;
+
+  return `${countV}`;
+}
+
+countC(jammer:any): string {
+  const countC = jammer.current / 1000;
+  return `${countC}`;
+}
+
+
+
+
+// Ambil data Json
+  getDataJson(){
+    this.Datajson = dashboardJammer;
+    this.Jammer1 = dashboardJammer.jammer1;
+    this.Jammer2 = dashboardJammer.jammer2;
+    console.log(dashboardJammer.jammer1);
+    for (const jam1 of this.Jammer1) {
+      const bwFreqStart: any = this.calculateBwFreqStart(jam1);
+      const bwFreqEnd: any = this.calculateBwFreqEnd(jam1);
+
+      console.log(bwFreqStart);
+    }
   }
 
+// Hitung - Hitungan
 
+
+
+//Modal Jammer
+  // Select button Group Modal jammer 1
   selectChannelJam1(channelJam1: number) {
-    this.channelJam1Tabs.forEach(tab => (tab.isActive = tab.channel === channelJam1));
-    this.selectJam1 = channelJam1; 
-    console.log('Selected Channel : ', channelJam1);
+    this.selectJam1 = channelJam1;
+    return this.selectJam1;
   }
 
+  // Select button Group Modal jammer 2
   selectChannelJam2(channelJam2: number) {
     this.channelJam2Tabs.forEach(tab => (tab.isActive = tab.channel === channelJam2));
     this.selectJam2 = channelJam2;
@@ -50,6 +128,7 @@ export class DashboardJammerPage implements OnInit {
 
   }
 
+//End Modal Jammer
   redirectToMainDashboard() {
     this.router.navigateByUrl('dashboard');
   }
